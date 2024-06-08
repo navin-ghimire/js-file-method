e373c600a742e6e5070a5b4137a887c600b03681
navin-ghimire <navinghimire143@gmail.com>
2024-06-08 14:23:48 +0545

    data

---
+++ index.js
@@ -0,0 +1,21 @@
+const personName = 'ram   ';
+const address = 'new road';
+const somes = 'hari is bad boy';
+
+
+console.log(somes.replace('bad', 'good'));
+
+
+
+
+
+//console.log(personName.trim().length)
+
+// console.log(personName.length);
+// console.log(personName.charAt(0));
+//console.log(personName.concat('myworld'));
+//console.log(personName, address.concat());
+//console.log(personName.includes('am'));
+//console.log(personName.repeat(3));
+//console.log(personName.toUpperCase())
+
